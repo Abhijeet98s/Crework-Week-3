59e6333e7d0d1f7bd1dea6426460a06526927cc4
Abhijeet Sharma <abhijeetsharma476@gmail.com>
2022-08-06 13:25:13 +0530

    added tailwind task

---
+++ P-1 Vanilla CSS/README.md
@@ -0,0 +1,10 @@
+# Created E-commerce Product Page
+
+## Reference Image:
+![](./desktop-preview.jpg)
+
+***
+
+## Desktop View:
+![](./screenshot/week-3.png)
+
---
+++ P-1 Vanilla CSS/js/main.js
@@ -0,0 +1,24 @@
+let counterDisplay = document.querySelector('.counter-display');
+let counterMinus = document.querySelector('.counter-minus');
+let counterPlus = document.querySelector('.counter-plus');
+
+let count = 0;
+
+
+counterPlus.addEventListener("click",()=>{
+    count++;
+    updateDisplay();
+}) ;
+
+counterMinus.addEventListener("click",()=>{
+    count--;
+    updateDisplay();
+});
+
+function updateDisplay(){
+    counterDisplay.innerHTML = count;
+};
+
+updateDisplay();
+
+
